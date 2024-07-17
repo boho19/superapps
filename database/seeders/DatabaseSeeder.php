ebdf6340b64e7fc97a15c0abf2ab516fbf436776
boho19 <husnimubarokh123@gmail.com>
2024-07-18 01:25:00 +0700

    sedeer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Karyawan::factory(3)->create();
-        Absen::factory(3)->create();
-        Izin::factory(3)->create();
+        // Karyawan::factory(3)->create();
+        // Absen::factory(3)->create();
+        // Izin::factory(3)->create();
+         
+        $user = new User();
+        $user->username= 'user@gmail.com';
+        $user->password= 'admin123';
+        $user->role= 'karyawan';
+        $user->save();
+
+
+        $user = new User();
+        $user->username= 'admin@gmail.com';
+        $user->password= 'admin123';
+        $user->role= 'admin';
+        $user->save();
+
 
         // User::factory()->create([
         //     'name' => 'Test User',
